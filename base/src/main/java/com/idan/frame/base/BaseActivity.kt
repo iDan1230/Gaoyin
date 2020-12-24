75027488bb944c323acd486496c2445b4834a838
iDan1230 <iDan1230@163.com>
2020-12-24 22:26:58 +0800

    沉浸式状态栏

--- base/src/main/java/com/idan/frame/base/BaseActivity.kt
+++ base/src/main/java/com/idan/frame/base/BaseActivity.kt
@@ -1,6 +1,11 @@
 package com.idan.frame.base
 
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -28,6 +33,7 @@ abstract class BaseActivity<DB : ViewDataBinding, VM : BaseViewModel> : AppCompa
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTranslucentStatus()
         //必须执行inject才能获取通过Arouter传递的参数
         ARouter.getInstance().inject(this)
         mDb.lifecycleOwner = this
@@ -68,4 +74,28 @@ abstract class BaseActivity<DB : ViewDataBinding, VM : BaseViewModel> : AppCompa
             loading.setMessage(fail)
         }
     }
+
+    private fun trsan() {
+        val view = window.decorView
+
+    }
+
+    open fun setTranslucentStatus() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            window.setDecorFitsSystemWindows(false)
+//           val wc = window.insetsController
+//            wc?.apply {
+//                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+//                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//            }
+//        }
+
+    }
+
 }
