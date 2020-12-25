68510bd179bfaf22a927af724896c2d95052fbbe
iDan1230 <iDan1230@163.com>
2020-12-25 17:51:47 +0800

    登录页面真加Arouter跳转成功后finish页面

--- app/src/main/java/com/gaoyin/app/LaunchActivity.kt
+++ app/src/main/java/com/gaoyin/app/LaunchActivity.kt
@@ -9,6 +9,7 @@ class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
-        startActivity(Intent(this,LoginActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
--- login/src/main/java/com/idan/login/ui/login/LoginActivity.kt
+++ login/src/main/java/com/idan/login/ui/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.idan.login.ui.login
 
 import androidx.lifecycle.Observer
+import com.alibaba.android.arouter.facade.Postcard
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.facade.callback.NavigationCallback
 import com.alibaba.android.arouter.launcher.ARouter
 import com.idan.frame.ID
 import com.idan.frame.TITLE
@@ -32,7 +34,20 @@ class LoginActivity : BaseActivity<ActivityLoginBinding,LoginViewModel>() {
             ARouter.getInstance().build("/main/main")
                 .withLong(ID,10010L)
                 .withString(TITLE,"TEST_TITLE")
-                .navigation()
+                .navigation(this,object :NavigationCallback{
+                    override fun onLost(postcard: Postcard?) {
+                    }
+
+                    override fun onFound(postcard: Postcard?) {
+                    }
+
+                    override fun onInterrupt(postcard: Postcard?) {
+                    }
+
+                    override fun onArrival(postcard: Postcard?) {
+                        finish()
+                    }
+                })
         })
     }
 }
